b1aa7f4c1935ca98abf3d0f80eed01fe1567ac2f
Matias <matias.nappa@metropolia.fi>
2023-11-25 10:21:17 +0200

    Re-did the script to make sense. Simulator can now be controlled from the web.

--- script/mqttSimulator.js
+++ script/mqttSimulator.js
@@ -1,5 +1,4 @@
 const mqtt = require('mqtt');
-
 const brokerUrl = 'mqtt://localhost';
 const options = {
   clientId: 'mqtt_simulator',
@@ -8,16 +7,42 @@ const options = {
 
 const client = mqtt.connect(brokerUrl, options);
 
+let soilMoisture = 100;
+let tankLevel = 100;
+let irrigate = false;
+let pumpStatus = false;
+
+function irrigateCheck() {
+  // conditions are met or pump is commanded
+  if (irrigate || pumpStatus) {
+    if (tankLevel > 0) {
+      soilMoisture = 100;
+      tankLevel -= 10; // water in the tank, use it
+    } else {
+      soilMoisture = 100;
+      tankLevel = 100; // tank empty, user refills tank
+    }
+    irrigate = false;
+    pumpStatus = false;
+  }
+}
+
 // Mock data
-function sendMockData() {
-  const pumpStatus = Math.random() < 0.5 ? 0 : 1;
-  const tankLevel = Math.floor(Math.random() * 100);
-  const soilMoisture = Math.floor(Math.random() * 100);
-  const temperature = Math.floor(Math.random() * 100);
-  const lightLevel = Math.floor(Math.random() * 100);
+function measure() {
+
+  if (soilMoisture > 0) {
+    soilMoisture -= 1;
+  }
+
+  const temperature = Math.floor(Math.random() * 16) + 15;
+  const lightLevel = Math.floor(Math.random() * 25000);
+
+  // example of user configuration/irrigation profile
+  // if (temperature >= 15 && soilMoisture < 50) irrigate = true;
+
+  irrigateCheck();
 
   const data = {
-    pumpStatus,
     tankLevel,
     soilMoisture,
     temperature,
@@ -30,9 +55,43 @@ function sendMockData() {
 client.on('connect', () => {
   console.log('MQTT ok');
 
+  client.subscribe('telefarm/irrigate', (err) => {
+    if (err) {
+      console.error('Subscription error:', err);
+    } else {
+      console.log('Subscribed to telefarm/irrigate');
+    }
+  });
+
+  client.subscribe('telefarm/pump', (err) => {
+    if (err) {
+      console.error('Subscription error:', err);
+    } else {
+      console.log('Subscribed to telefarm/pump');
+    }
+  });
+
   setInterval(() => {
-    sendMockData();
-  }, 5000); // 5 secs
+    measure();
+  }, 1000);
+});
+
+// Handle control messages from the UI
+client.on('message', (topic, message) => {
+  if (topic === 'telefarm/irrigate') {
+    const payload = message.toString();
+    if (payload === 'true') {
+      irrigate = !irrigate;
+      console.log(`Irrigation control message: ${payload}. Toggled irrigate to ${irrigate}`);
+    }
+  }
+  else if (topic === 'telefarm/pump') {
+    const payload = message.toString();
+    if (payload === 'true') {
+      pumpStatus = !pumpStatus;
+      console.log(`Pump control message: ${payload}. Toggled pump to ${pumpStatus}`);
+    }
+  }
 });
 
 client.on('error', (error) => {
--- server.js
+++ server.js
@@ -5,10 +5,9 @@ const mqtt = require('mqtt');
 const path = require('path');
 
 const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
-
-let latestSensorData = {};
+const wss = new WebSocket.Server({server});
 
 const brokerUrl = 'mqtt://localhost';
 const options = {
@@ -17,6 +16,7 @@ const options = {
 };
 
 const client = mqtt.connect(brokerUrl, options);
+let latestSensorData = {};
 
 // measurements published on telefarm/status
 client.on('connect', () => {
@@ -42,9 +42,31 @@ wss.on('connection', (ws) => {
   ws.send(JSON.stringify(latestSensorData));
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.post('/pump', (req, res) => {
+  client.publish('telefarm/pump', 'true', function(err) {
+    if (err) {
+      console.error('Error publishing message:', err);
+      res.status(500).send('Error while triggering pump manually');
+    } else {
+      console.log('Published "true" to telefarm/pump');
+      res.status(200).send('Pump triggered successfully');
+    }
+  });
+});
+
+app.post('/irrigate', (req, res) => {
+  client.publish('telefarm/irrigate', 'true', function(err) {
+    if (err) {
+      console.error('Error publishing message:', err);
+      res.status(500).send('Error when calling for irrigation');
+    } else {
+      console.log(`Plant has been watered at [${new Date().toLocaleString()}]`);
+    }
+  });
 });
 
 server.listen(3000, () => {
